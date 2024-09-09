81607e4c0729470a11a6df7efc752d79e97364a4
dilsher07singh <dilsher07singh@gmail.com>
2024-09-09 13:07:13 +0530

    Updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa"; // Import WhatsApp icon
 import { FaInstagram } from "react-icons/fa";
 import logo from "../assets/dilsherSinghLogo.webp";
 
@@ -11,10 +11,34 @@ const Navbar = () => {
         <img className="mx-2 w-20" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <a
+          href="https://www.linkedin.com/in/dilsher07singh/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/dilsher07singh"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.instagram.com/dilsher.07/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://wa.me/919740071441"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaWhatsapp />
+        </a>
       </div>
     </nav>
   );
