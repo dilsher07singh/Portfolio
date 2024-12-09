77c9cd8b13a3abd51927fa0eb55bf4a8db5d17b0
dilsher07singh <dilsher07singh@gmail.com>
2024-12-09 12:43:20 +0530

    Updated about image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,10 @@
-import aboutImg from "../assets/about.jpg";
 import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
 
 const About = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4">
-      <h1 className="my-20 text-center text-4xl">
+    <div className="pb-4 border-b border-neutral-900">
+      <h1 className="my-20 text-4xl text-center">
         About <span className="text-neutral-500">Me</span>
       </h1>
       <div className="flex flex-wrap">
@@ -16,7 +15,7 @@ const About = () => {
           className="w-full lg:w-1/2 lg:p-8"
         >
           <div className="flex items-center justify-center">
-            <img className="rounded-2xl" src={aboutImg} alt="about" />
+            <img className="rounded-2xl" src={"/"} alt="about" />
           </div>
         </motion.div>
         <motion.div
